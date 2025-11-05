7255ce66795ea047ea4e29eb8cda4e9ec9876117
Harsh <harshlad@492002@gmail.com>
2025-11-05 10:45:22 +0530

    add all eslint, prettier and ci changes

--- server/middlewares/error-middleware.js
+++ server/middlewares/error-middleware.js
@@ -1,7 +1,8 @@
-const errorMiddleware = (err, req, res) => {
+const errorMiddleware = (err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || 'BACKEND ERROR';
-  const extraDetails = err.extraDetails || 'Error form Backend';
+  const extraDetails = err.extraDetails || 'Error from Backend';
+
   return res.status(status).json({ message, extraDetails });
 };
 
